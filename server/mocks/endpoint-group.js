3611638faa9e5f0b885e9812c6674b96491e1d2a
rmorey-ap <rmorey@anypresence.com>
2015-01-27 11:29:07 -0800

    added endpoint group mocks

---
+++ server/mocks/endpoint-group.js
@@ -0,0 +1,58 @@
+module.exports = function(app) {
+  var express = require('express');
+  var endpointGroupRouter = express.Router();
+  var groups = [
+    {
+      id: 1,
+      name: 'Group A',
+      description: 'This group is named "A"'
+    },
+    {
+      id: 2,
+      name: 'Group B',
+      description: 'This group is named "B"'
+    },
+    {
+      id: 3,
+      name: 'Group C',
+      description: 'This group is named "C"'
+    }
+  ];
+  
+  endpointGroupRouter.get('/', function(req, res) {
+    res.send({
+      'endpoint_groups': groups
+    });
+  });
+
+  endpointGroupRouter.post('/', function(req, res) {
+    var body = req.body;
+    var id = Math.round(Math.random() * 100);
+    body.endpoint_group.id = id;
+    res.status(201).send(body).end();
+  });
+
+  endpointGroupRouter.get('/:id', function(req, res) {
+    res.send({
+      'endpoint_group': {
+        id: req.params.id
+      }
+    });
+  });
+
+  endpointGroupRouter.put('/:id', function(req, res) {
+    var body = req.body;
+    body.endpoint_group.id = req.params.id;
+    if (body.endpoint_group.name.toLowerCase() == 'error') {
+      res.status(422).send({errors: {name: 'This field is in error.'}})
+    } else {
+      res.send(body);
+    }
+  });
+
+  endpointGroupRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/admin/apis/:api_id/endpoint_groups', endpointGroupRouter);
+};
