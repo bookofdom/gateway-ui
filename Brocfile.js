ed2bb3a38fd7791f131855be83cdbf4167eb7266
rmorey-ap <rmorey@anypresence.com>
2015-03-16 10:16:13 -0700

    [#90416504] added gzipping

--- Brocfile.js
+++ Brocfile.js
@@ -4,7 +4,11 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app'),
   pickFiles = require('broccoli-static-compiler'),
   mergeTrees = require('broccoli-merge-trees');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  gzip: {
+    extensions: ['js', 'css', 'svg', 'png']
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
